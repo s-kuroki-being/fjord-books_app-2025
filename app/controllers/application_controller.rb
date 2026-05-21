c1d3b54323039f222ebef8c3c38dc4a5b285b210
s-kuroki <s-kuroki@beingcorp.co.jp>
2026-05-21 18:17:14 +0900

    書籍登録アプリ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has
   allow_browser versions: :modern
+
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end

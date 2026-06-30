a7ac2ed02e81aa9c487a12f86526bbbadd034a71
s-kuroki <s-kuroki@beingcorp.co.jp>
2026-06-30 10:40:03 +0900

    fix: 自己紹介文が更新できない現象の改善。アカウント登録画面の見出しを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :profile])
   end
 end

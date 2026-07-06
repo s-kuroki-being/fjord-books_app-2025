2ebd83de8963aed4ff8f89ca9df7686e4673bdad
s-kuroki <s-kuroki@beingcorp.co.jp>
2026-07-06 17:28:33 +0900

    config.mailer_senderをダミーアドレスに変更。不要なja.ymlの削除。DBカラム名の変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 end
